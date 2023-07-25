7838ef64bc3ce9470d88819feadece1d0d32fc62
Yohann Assouline <yohannassouline@hotmail.fr>
2023-07-25 15:00:55 +0200

    Add cli parser
    Add multiples cli features:
    - override host from cli arg
    - add or override global headers from cli args
    - change verbose level from cli arg

--- api-tester.py
+++ api-tester.py
@@ -6,10 +6,11 @@ import json
 from typing import List, Dict
 import requests
 import colored
+import argparse
 from colored import stylize
 
-# 0 Normal | 1 Quiet | 2 Super Quiet
-QUIET_LEVEL = 0
+# 2 Default | 1 Quiet
+VERBOSE_LEVEL = 2
 
 METHOD_NAME_TO_FUNCTION = {
     'GET': requests.get,
@@ -19,7 +20,6 @@ METHOD_NAME_TO_FUNCTION = {
     'PATCH': requests.patch,
     'DELETE': requests.delete,
 }
-
 
 class HttpTester:
     def __init__(self, host: str, test_list: List[object], global_headers: Dict[str, str]):
@@ -34,7 +34,11 @@ class HttpTester:
 
     def print_test_fail(self, msg: str):
         self.nb_tests_failed += 1
-        print(f"{msg} {stylize('KO', colored.fg('red'))}")
+        if VERBOSE_LEVEL == 2:
+            # TODO, print better diff
+            print(f"{msg} {stylize('KO', colored.fg('red'))}")
+        else:
+            print(f"{stylize('KO', colored.fg('red'))}")
 
     def run(self):
         for test in self.test_list:
@@ -81,16 +85,10 @@ class HttpTester:
 
     def print_summary(self):
         """ print a summary of the tests results """
-        if QUIET_LEVEL == 2:
-            if self.nb_tests_failed > 0:
-                print(f"{self.host}: {stylize('KO', colored.fg('red'))}")
-            else:
-                print(f"{self.host}: {stylize('OK', colored.fg('green'))}")
-        else:
-            count_success = self.nb_tests - self.nb_tests_failed
-            print(f"\nSummary {self.host}: {self.nb_tests} tests ran")
-            print(f"{count_success} : {stylize('OK', colored.fg('green'))}")
-            print(f"{self.nb_tests_failed} : {stylize('KO', colored.fg('red'))}")
+        count_success = self.nb_tests - self.nb_tests_failed
+        print(f"\nSummary {self.host}: {self.nb_tests} tests ran")
+        print(f"{count_success} : {stylize('OK', colored.fg('green'))}")
+        print(f"{self.nb_tests_failed} : {stylize('KO', colored.fg('red'))}")
 
     def prepare_test(self, test):
         # method
@@ -123,18 +121,82 @@ class HttpTester:
         return method, full_url, params, headers, body
 
 
-def main(argc: int, argv: List[str]) -> None:
-    if argc != 2:
-        sys.exit(f"USAGE {argv[0]} test_file.json")
+class CliParser:
+    def __init__(self):
+        pass
+
+    def __new__(cls):
+        parser = argparse.ArgumentParser(
+            prog='api-tester',
+            description=' A python program to test an api based on a json configuration file'
+        )
+        parser.add_argument('test_file')
+        parser.add_argument('-v', '--verbose-level', choices=['2', '1'], default='2',
+                            help='2: default verebosity level that prints everything, 1: hide test diff when a test fails'
+                            )
+        parser.add_argument(
+            '--host', help='Override host values in json config file')
+        parser.add_argument("--headers",
+                            metavar="KEY=VALUE",
+                            nargs='+',
+                            help="headers"
+                            "Add values into global headers"
+                            "(do not put spaces before or after the = sign). "
+                            "If a value contains spaces, you should define it with double quotes: ")
+
+        args = parser.parse_args()
+        return {
+            'test_file': args.test_file,
+            'host': args.host,
+            'verbose_level': args.verbose_level,
+            'headers': CliParser.parse_vars(args.headers),
+        }
+
+    def parse_var(s):
+        """
+        Parse a key, value pair, separated by '='
+
+        On the command line (argparse) a declaration will typically look like:
+            foo=hello
+        or
+            foo="hello world"
+        """
+        items = s.split('=')
+        key = items[0].strip()  # remove blanks around keys
+        if len(items) > 1:
+            # rejoin the rest:
+            value = '='.join(items[1:])
+        return (key, value)
+
+    def parse_vars(items):
+        """
+        Parse a series of key-value pairs and return a dictionary
+        """
+        d = {}
+        if items:
+            for item in items:
+                key, value = CliParser.parse_var(item)
+                d[key] = value
+        return d
+
+
+def main() -> None:
+    global VERBOSE_LEVEL
+
+    cli_args = CliParser()
+
+    VERBOSE_LEVEL = int(cli_args['verbose_level'])
 
     json_content = None
-    with open(argv[1], "r") as f:
+    with open(cli_args['test_file'], "r") as f:
         json_content = json.loads(f.read())
 
     # get host
     if 'host' not in json_content:
         sys.exit("Missing 'host' global key")
     host = json_content['host']
+    if cli_args['host'] != None:
+        host = cli_args['host']
 
     # get test list
     if 'tests' not in json_content:
@@ -146,10 +208,12 @@ def main(argc: int, argv: List[str]) -> None:
         headers = json_content['headers']
     else:
         headers = {}
+    if cli_args['headers'] != None:
+        headers = {**headers, **cli_args['headers']}
 
     tester = HttpTester(host, tests, headers)
     return tester.run()
 
 
 if __name__ == "__main__":
-    sys.exit(main(len(sys.argv), sys.argv))
+    sys.exit(main())
